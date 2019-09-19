8e80cd21b6d0bc02a4791319c97d6bde144da753
Reza Shisheie <reza.shishei@gmail.com>
2019-09-19 16:58:02 -0400

    Add files via upload

---
+++ Homework#10_lecture_11_rule_based/lecture_11_rule_based/GestureBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+
+namespace lecture_11_rule_based
+{
+
+    public abstract class GestureBase
+    {
+        public GestureBase(GestureType type)
+        {
+            this.CurrentFrameCount = 0;
+            this.GestureType = type;
+        }
+        public bool IsRecognitionStarted { get; set; }
+        private int CurrentFrameCount { get; set; }
+        public GestureType GestureType { get; set; }
+        protected virtual int MaximumNumberOfFrameToProcess
+        {
+            get { return 15; }
+        }
+        public long GestureTimeStamp { get; set; }
+        protected abstract bool ValidateGestureStartCondition(Skeleton skeleton);
+        protected abstract bool ValidateGestureEndCondition(Skeleton skeleton);
+        protected abstract bool ValidateBaseCondition(Skeleton skeleton);
+        protected abstract bool IsGestureValid(Skeleton skeleton);
+
+        public virtual bool CheckForGesture(Skeleton skeleton)
+        {
+            if (this.IsRecognitionStarted == false)
+            {
+                if (this.ValidateGestureStartCondition(skeleton))
+                {
+                    this.IsRecognitionStarted = true;
+                    this.CurrentFrameCount = 0;
+                }
+            }
+            else
+            {
+                if (this.CurrentFrameCount == this.MaximumNumberOfFrameToProcess)
+                {
+                    this.IsRecognitionStarted = false;
+                    if (ValidateBaseCondition(skeleton) && ValidateGestureEndCondition(skeleton))
+                    { return true; }
+                }
+                this.CurrentFrameCount++;
+                if (!IsGestureValid(skeleton) && !ValidateBaseCondition(skeleton))
+                {
+                    this.IsRecognitionStarted = false;
+                }
+            }
+            return false;
+        }
+
+    }
+}
+
---
+++ Homework#10_lecture_11_rule_based/lecture_11_rule_based/GestureHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace lecture_11_rule_based
+{
+    class GestureHelper
+    {
+        public static float GetJointDistance(Joint firstJoint, Joint secondJoint)
+        {
+            float distanceX = firstJoint.Position.X - secondJoint.Position.X;
+            float distanceY = firstJoint.Position.Y - secondJoint.Position.Y;
+            float distanceZ = firstJoint.Position.Z - secondJoint.Position.Z;
+            return (float)Math.Sqrt(Math.Pow(distanceX, 2) + Math.Pow(distanceY, 2) + Math.Pow(distanceZ, 2));
+        }
+    }
+}
