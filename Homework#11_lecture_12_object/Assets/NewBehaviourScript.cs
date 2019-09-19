634ed15e3cac98d52e76fd32eb50823890c9bd23
Reza Shisheie <reza.shishei@gmail.com>
2019-09-19 16:59:11 -0400

    Add files via upload

---
+++ Homework#11_lecture_12_object/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(0,5 * Time.deltaTime,0);
+		//transform.Translate(1 * Time.deltaTime,0,0);
+	
+	}
+}
