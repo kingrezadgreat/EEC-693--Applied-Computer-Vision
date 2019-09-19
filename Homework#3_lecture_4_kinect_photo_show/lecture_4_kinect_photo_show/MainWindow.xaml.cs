ecff735472a0cb5b779fdd2ababafa748d870e67
Reza Shisheie <reza.shishei@gmail.com>
2019-09-19 16:56:31 -0400

    Add files via upload

---
+++ Homework#3_lecture_4_kinect_photo_show/lecture_4_kinect_photo_show/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture_4_kinect_photo_show
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor sensor;
+        private WriteableBitmap colorBitmap;
+        private byte[] colorPixels;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.sensor = KinectSensor.KinectSensors[0]; if (this.sensor != null && !this.sensor.IsRunning)
+                {
+                    this.sensor.ColorStream.Enable(); this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                    this.image1.Source = this.colorBitmap;
+                    this.sensor.ColorFrameReady += this.colorFrameReady;
+                    this.sensor.Start();
+                }
+                else
+                {
+                    MessageBox.Show("No device is connected!"); this.Close();
+                }
+            }
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.sensor != null && this.sensor.IsRunning)
+            {
+                this.sensor.Stop();
+            }
+        }
+        void colorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame imageFrame = e.OpenColorImageFrame())
+            {
+                if (null == imageFrame) return;
+                imageFrame.CopyPixelDataTo(colorPixels);
+                int stride = imageFrame.Width * imageFrame.BytesPerPixel;
+                // Write the pixel data into our bitmap 
+                this.colorBitmap.WritePixels( new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight), this.colorPixels,stride, 0);
+            }
+        }
+    }
+}
